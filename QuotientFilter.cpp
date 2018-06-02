b8d545652a924cb4c14a458505338530dbd7367c
Nathan <nathanbutler@Nathans-Personal.local>
2018-06-02 13:34:29 -0700

    Quotient filter working

--- QuotientFilter.cpp
+++ QuotientFilter.cpp
@@ -12,6 +12,7 @@ QuotientFilter::QuotientFilter(size_t numBuckets,
   q = 16;
   std::vector<bool> stat_temp(3*numBuckets, false);
   stat_arr = stat_temp;
+  num_elems = 0;
 }
 
 QuotientFilter::~QuotientFilter() {
@@ -19,7 +20,6 @@ QuotientFilter::~QuotientFilter() {
 }
 
 int QuotientFilter::getqr(int f, bool is_q) const {
-  // TODO: Implement this
   int mask = 0xffffffff;
   int ret = 0;
   if(is_q){
@@ -34,104 +34,97 @@ int QuotientFilter::getqr(int f, bool is_q) const {
 }
 
 int QuotientFilter::insert(int data) {
-  /* Pseudo Code: 
-   *    Scratch that, do what contains 
-   *    explains and shift everything
-   *    (actually much more similar to
-   *    robin hood hashing)
-   * */
-
-
   int f = fp(data);
   int q_int = getqr(f, true);
   int r_int = getqr(f, false);
   size_t bucket = q_int % numBucks;
-  //std::cout<<"BUCKET VALUE: "<<bucket<<std::endl;
+  
+  // If full, cant insert.
+  if(num_elems == numBucks)
+      return -1;
 
   // If bucket empty, place.
   if(!isFilled(bucket)) {
-    //std::cout<<"No Eviction"<<std::endl;
-    //std::cout<<"INSERTING INTO: "<<bucket<<std::endl;
+    num_elems += 1;
     buckets[bucket] = r_int;
     set_3_bit(bucket, true, false, false);
     return 1;
   }
-
-  // Now let's scan through run
-  std::vector<size_t> cluster_info = scan_left(bucket);
-  size_t cluster_start = cluster_info[0]; // Start of the cluster
-  size_t runs_before = cluster_info[1];   // Number of runs before the one we want
-  bool first_in_run = !stat_arr[bucket*3]; // If our run already exists..
-  /**std::cout<<"AFTER SCAN LEFT: "<<std::endl;
-  std::cout<<"CLUSTER START: "<<cluster_start<<std::endl;
-  std::cout<<"RUNS BEFORE: "<<runs_before<<std::endl;
-  std::cout<<"BUCKET: "<<bucket<<std::endl;
-  */
-  // If cluster_start is > numBucks, then saturated and done inserting...
-  if(cluster_start > numBucks)
-    return -1;
-
-  // Scan right now
-  size_t run_start = scan_right(cluster_start, runs_before);
+  
+  bool first_in_run = !stat_arr[bucket*3];
+  
+  // After scans, make sure to set connonical bit:
+  stat_arr[bucket*3] = true;
+  size_t run_start = find_run(bucket);
+  //std::cout<<"FOUND RUN"<<std::endl;
   bool inserted = false;
   bool last_cont = !first_in_run;
   bool run_over = false;
   size_t started = run_start;
-
+  
   // After scans, make sure to set connonical bit:
   stat_arr[bucket*3] = true;
   //std::cout<<"RUN START: "<<run_start<<std::endl;
-  //if(stat_arr[(run_start * 3) + 1])
-  //    std::cout<<"IS CONTINUATION"<<std::endl;
-  // Loop while still looking at non-empty spots
   while(isFilled(run_start) == true) {
-    // If we hit value, return right away
-    if(buckets[run_start] == r_int)
-      return 1;
-    // If slot where r_int goes and have not inserted, evict
-    if((buckets[run_start] > r_int) || inserted || first_in_run || run_over){
-      int temp = buckets[run_start];
-      buckets[run_start] = r_int;
-      bool temp_cont = stat_arr[(run_start * 3) + 1];
-      if(!inserted){
-        //std::cout<<"...."<<std::endl;
-        //std::cout<<"INSERTING INTO: "<<run_start<<std::endl;
-        //std::cout<<"CLUSTER START: "<<cluster_start<<std::endl;
-        //std::cout<<"BUCKET VAL: "<<bucket<<std::endl;
-        //std::cout<<"RUNS BEFORE: "<<runs_before<<std::endl;
-        //std::cout<<"...."<<std::endl;
-        if(first_in_run) {
-          stat_arr[(run_start*3) + 1] = false; // set cont to false
-          stat_arr[(run_start*3) + 2] = (bucket != run_start);  // Set shifted if shifted
-          temp_cont = false;
-        }else {
-          stat_arr[(run_start*3) + 1] = true; // set cont to true
-          stat_arr[(run_start*3) + 2] = true; // set shifted to true
-        }
+    if(!inserted) {
+        int temp = buckets[run_start];
         inserted = true;
-      }else {
+        // If we are the first element in the run
+        if(first_in_run) {
+    //        std::cout<<"INSERTING INTO: "<<run_start<<std::endl;
+            stat_arr[(run_start*3) + 1] = false;
+            stat_arr[(run_start*3) + 2] = (bucket != run_start);
+            buckets[run_start] = r_int;
+            r_int = temp;
+            last_cont = false;
+        }else if(run_over) { 
+            
+      //      std::cout<<"INSERTING INTO: "<<run_start<<std::endl;
+            stat_arr[(run_start*3) + 1] = true;
+            stat_arr[(run_start*3) + 2] = true;
+            buckets[run_start] = r_int;
+            r_int = temp;
+            last_cont = false;
+        }else if((buckets[run_start] > r_int)) {
+        //    std::cout<<"INSERTING INTO: "<<run_start<<std::endl;
+            //stat_arr[(run_start*3) + 1] = true;
+            //stat_arr[(run_start*3) + 2] = (bucket != run_sta;
+            buckets[run_start] = r_int;
+            r_int = temp;
+            last_cont = true;
+        } else if(buckets[run_start] == r_int) {
+            return 1;
+        }else {
+            inserted = false;
+        }
+    } else {
+        // save state
+        int temp = buckets[run_start];
+        bool temp_cont = stat_arr[(run_start*3) + 1];
         stat_arr[(run_start*3) + 1] = last_cont;
         stat_arr[(run_start*3) + 2] = true;
-      }
-      last_cont = temp_cont;
-      r_int = temp;
+        // shift back
+        buckets[run_start] = r_int;
+        r_int = temp;
+        last_cont = temp_cont;
     }
     run_start = increment(numBucks, run_start);
-    // To catch infinite loops on saturation...
     if(run_start == started)
         return -1;
-    if(!run_over)
-        run_over = !stat_arr[(run_start*3) + 1]; // If cont false, run is over and we need to insert
+    if(stat_arr[(run_start*3) + 1] == false)
+        run_over = true;
   }
-  
-  //if(!inserted) {
-  //  std::cout<<"INSERTING INTO: "<<run_start<<std::endl;
-  //}
   
   // Insert
   buckets[run_start] = r_int;
   stat_arr[(run_start*3) + 1] = last_cont;
-  stat_arr[(run_start*3) + 2] = true;
+  if(inserted){
+    stat_arr[(run_start*3) + 2] = true;
+  } else {
+    stat_arr[(run_start*3) + 2] = (bucket != run_start);
+  }
+    
+  num_elems += 1;
   return 1;
 }
 
@@ -149,9 +142,12 @@ bool QuotientFilter::contains(int data) const {
       //std::cout<<"Connonical slot empty"<<std::endl;
       return false;
   }
+  //std::cout<<"num_elems: "<<num_elems<<std::endl;
+  if(num_elems == numBucks)
+      return linscan(data);
 
   // If we have not shifted, run starts at spot.
-  if(stat_arr[(bucket*3) + 2] == true) {
+  if(!isClusterStart(bucket)) {
     //std::cout<<"SHIFTED"<<std::endl;
     // Now let's scan through run
     std::vector<size_t> cluster_info = scan_left(bucket);
@@ -159,7 +155,7 @@ bool QuotientFilter::contains(int data) const {
     size_t runs_before = cluster_info[1];
     
     // Subtract one off of runs before to account for own run
-    runs_before -= 1;
+    //runs_before -= 1;
 
     // If cluster_start is > numBucks, then saturated...
     if(cluster_start > numBucks){
@@ -174,11 +170,13 @@ bool QuotientFilter::contains(int data) const {
     //std::cout<<"AFTER SCAN LEFT IN CONTAINS: "<<std::endl;
     //std::cout<<"CLUSTER START: "<<cluster_start<<std::endl;
     //std::cout<<"RUNS BEFORE: "<<runs_before<<std::endl;
-    //std::cout<<"BUCKET: "<<bucket<<std::endl;
-    //std::cout<<"RUN START: "<<run_start<<std::endl;
   }
+  //std::cout<<"CONTAINS"<<std::endl;
+  //std::cout<<"BUCKET: "<<bucket<<std::endl;
+  //std::cout<<"RUN START: "<<run_start<<std::endl;
   //std::cout<<"RUN START: "<<run_start<<std::endl;  
   // Loop while still looking at non-empty spots
+  run_start = find_run(bucket);
   while(isFilled(run_start) == true) {
     // If we hit value, true!!
     if(buckets[run_start] == r_int)
@@ -186,6 +184,7 @@ bool QuotientFilter::contains(int data) const {
     // Because we are in sorted order, stop early if greater
     if(buckets[run_start] > r_int)
       return false;
+    
     run_start = increment(numBucks, run_start);
 
     // If our run is over, return false
@@ -212,8 +211,8 @@ bool QuotientFilter::linscan(int data) const{
     //    std::cout<<"INDEX NOT FILLED: "<<i<<std::endl;
     //}
   }
-/*
-  for(int j = 0; j < numBucks; j++) {
+
+  /*for(int j = 0; j < numBucks; j++) {
     std::string out  = "";
     out.resize(7);
     for(int k = 0; k < 3; k++){
@@ -275,8 +274,11 @@ std::vector<size_t> QuotientFilter::scan_left(size_t ind) const{
    */
   size_t cluster_start = ind;
   size_t runs = 0;
+  if(num_elems == numBucks)
+      return {numBucks + 1, runs};
+  
   // Loop non-empty
-  while(isFilled(cluster_start)) {
+  while(!isClusterStart(cluster_start)) {
       if(stat_arr[(cluster_start *3)] == true)
           runs += 1;
       cluster_start = decrement(cluster_start);
@@ -285,8 +287,9 @@ std::vector<size_t> QuotientFilter::scan_left(size_t ind) const{
       }
   }
   // Fence post.  Need to re increment cluster to get start
-  cluster_start = increment(numBucks, cluster_start);
-
+  //cluster_start = increment(numBucks, cluster_start);
+  if(stat_arr[(ind * 3) + 1] == false)
+      runs -= 1;
   // Subtract one off of runs to account for your own run
   return {cluster_start, runs};
 }
@@ -329,3 +332,29 @@ size_t QuotientFilter::increment(size_t numBucks, size_t bucket) const {
     return 0;
   return bucket + 1;
 }
+
+bool QuotientFilter::isClusterStart(size_t bucket) const {
+  return (stat_arr[bucket*3] == true && stat_arr[(bucket*3) + 1] == false
+          && stat_arr[(bucket*3) + 2] == false);
+}
+
+size_t QuotientFilter::find_run(size_t bucket) const{
+  size_t b = bucket;
+  while(stat_arr[(b*3) + 2] == true) {
+    b = decrement(b);
+  }
+  //std::cout<<"Cluster start: "<<b<<std::endl;
+  size_t s = b;
+  while(b != bucket) {
+    do {
+        s = increment(numBucks, s);
+    } while(stat_arr[(s*3) + 1] == true);
+
+    do {
+        b = increment(numBucks, b);
+    } while(!stat_arr[(b*3)]);
+
+  }
+  return s;
+}
+
--- QuotientFilter.h
+++ QuotientFilter.h
@@ -47,6 +47,9 @@ public:
   bool isFilled(size_t ind) const;
   size_t scan_right(size_t ind, size_t runs) const;
   size_t increment(size_t numBucks, size_t bucket) const;
+  bool isClusterStart(size_t bucket) const;
+  size_t find_run(size_t bucket) const;
+
 private:
   /* Fun with C++: these next two lines disable implicitly-generated copy
    * functions that would otherwise cause weird errors if you tried to
@@ -56,6 +59,7 @@ private:
   // Array of buckets containing data
   std::vector<int>  buckets;
   size_t numBucks;
+  size_t num_elems;
   HashFunction fp;
   int r;
   int q;
