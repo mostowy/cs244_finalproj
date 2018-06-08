776b0337defa9ec27aebb000c61b11ed50caf930
Nathan <nathanbutler@Nathans-Personal.local>
2018-06-08 12:43:36 -0700

    added look up and insert stuff from other git repo

--- Timing.h
+++ Timing.h
@@ -45,18 +45,25 @@ void init_keys() {
   std::cout<<"Randomly generated "<<num_keys<<" keys."<<std::endl;  
 }
 
+
 /**
- * Run insert benchmark tst
+ * Run insert benchmark test.  Filter type indicates the type of filter.
+ * By default it is cuckoo, set 1 for Quotient Filter, set 2 for bloom.
  */
 template <typename HT>
-void time_inserting(size_t buckets, std::shared_ptr<HashFamily> family) {
+void time_inserting(int filter_type, std::shared_ptr<HashFamily> family) {
   std::cout<<"Getting insert performance"<<std::endl;
   init_keys();
-  
-  HT table(buckets, family);
+  size_t bucket_size = num_rows;
+  switch(filter_type) {
+    case 1: bucket_size = (1 << 27);
+            break;
+    default: break;
+  }
+ 
+  HT table(bucket_size, family);
   size_t i = 0;
   Timer insertTimer;
-  // Do timing info here
   insertTimer.start();
   while((i < num_keys)) {
     if(table.insert(keys[i]) == -1)
@@ -64,9 +71,60 @@ void time_inserting(size_t buckets, std::shared_ptr<HashFamily> family) {
     i++;
   }
   insertTimer.stop();
-  auto time = insertTimer.elapsed();
-  std::cout<<"Inserting done after "<<i<<" elems in "<<time<<" nano seconds "<<std::endl;
-  // do info stuff
+  auto time = insertTimer.elapsed() * 1000000;
+  std::cout<<"Inserting done after "<<i<<" elems in "<<time<<" seconds "<<std::endl;
+  // do info stuf
+  time_lookup(filter_type, family, table);
+}
+
+/**
+ * Run lookup benchmark test.  Filter type indicates the type of filter.
+ * By default it is cuckoo, set 1 for Quotient Filter, set 2 for bloom.
+ */
+template <typename HT>
+void time_lookup(int filter_type, std::shared_ptr<HashFamily> family, HT table){
+    std::cout<<"Getting lookup performance"<<std::endl;
+    float pos_frac = .5;
+    size_t max_queries = 10000000;
+    auto queries = new uint64_t[max_queries];
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_int_distribution<long long unsigned> distribution(0,0xFFFFFFFFFFFFFFFF);
+    for (size_t i = 0; i < max_queries; i++) {
+        float  r = (double) rand() / RAND_MAX;
+        if (r  > pos_frac) {
+            queries[i] = distribution(generator);
+        } else {
+            // this is a positive query
+            size_t j = rand() % num_keys;
+            queries[i] = keys[j];
+        }
+    }
+
+    size_t false_ops  = 0;
+    size_t total_ops = 0;
+    double total_time = 0;
+    while (total_time < 10) 
+    {
+
+        Timer querytimer;
+        querytimer.start();
+        for (size_t j = 0; j < max_queries; j++) {
+            int res = (table.contains(queries[j]) == true);
+            false_ops += res;
+        }
+        querytimer.stop();
+
+        auto time = querytimer.elapsed();
+        total_time += time * 1000000;
+        total_ops += max_queries;
+    }
+
+    std::cout <<  "[lookup] complete querying " << total_ops << " keys in " << total_time <<"sec\n";
+    printf("[lookup] %.2f M queries / second,  %.2f ns,  f.p.r. = %f\n",
+           ((double)(total_ops)/(total_time *1000000)),
+           ((double) total_time) * 1000000000 / total_ops,
+           1.0 * false_ops / total_ops);
 }
 
 
@@ -131,9 +189,9 @@ template <typename HT>
 bool checkCorrectness(std::initializer_list<std::shared_ptr<HashFamily>> families) {
   for (auto family: families) {
     if (!checkCorrectness<HT>({
-//          std::make_tuple(12, family, 5),
-//            std::make_tuple(120, family, 50),
-            std::make_tuple(84769091, family, 5000)
+          std::make_tuple(12, family, 5),
+            std::make_tuple(120, family, 50),
+            std::make_tuple(1200, family, 5000)
             })) {
       return false;
     }
