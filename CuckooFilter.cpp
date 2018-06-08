dfd469511e4764c21ded2ba3d27e4ffc1a5f1fe6
Nathan <nathanbutler@Nathans-Personal.local>
2018-06-08 11:42:36 -0700

    I think specs matching paper for Cuckoo filter.  Also seeing a false positive rate similar to the one they have which is promising

--- CuckooFilter.cpp
+++ CuckooFilter.cpp
@@ -8,21 +8,34 @@
 
 CuckooFilter::CuckooFilter(size_t num_buckets, std::shared_ptr<HashFamily> family):
                            buckets(num_buckets){
-  //size_t numBuckets = 4; 
-  // First set variables  
   numBucks = num_buckets;
   h1 = family->get();
   finger_pointer = family->get();
   num_max_cuckoos = 500;
   num_elems = 0;
-  // Now set b1 and b2
-
+  f_bit_size = 12;
+  f_mask = (1ULL << f_bit_size) - 1;
 }
 
 CuckooFilter::~CuckooFilter() {
 }
 
-int CuckooFilter::run_cuckoo_loop(int f, int ind1, int ind2) {
+uint32_t CuckooFilter::get_finger_print(uint64_t data) const{
+    // Problem here is that hash is 64 but we want
+    // a specific finger print size.  For us, that
+    // size is 12 for the experiment
+    uint64_t large_finger = finger_pointer(data);
+    
+    // Get 32 most significant bits, mask off f
+    uint32_t f = (large_finger >> 32) & f_mask;
+    
+    // Make sure finger print is not zero
+    f += (f == 0);
+    return f;
+}
+
+
+int CuckooFilter::run_cuckoo_loop(uint32_t f, int ind1, int ind2) {
   //std::cout<<"Running Cuckoo Loop"<<std::endl;
   int counter = rand() % 2;
   int ind = (counter % 2 == 0) ? ind2 : ind1;
@@ -39,8 +52,8 @@ int CuckooFilter::run_cuckoo_loop(int f, int ind1, int ind2) {
   return -1;
 }
 
-int CuckooFilter::insert(int data) {
-  int f = finger_pointer(data);
+int CuckooFilter::insert(uint64_t data) {
+  uint32_t f = get_finger_print(data);
   int ind1 = h1(data) % numBucks;
   int ind2 = (ind1 ^ h1(f)) % numBucks;
  // std::cout<<"Ind 1: "<<ind1<<" Ind 2: "<<ind2<<std::endl;
@@ -67,15 +80,15 @@ int CuckooFilter::insert(int data) {
   return run_cuckoo_loop(f, ind1, ind2);
 }
 
-bool CuckooFilter::contains(int data) const {
-  int f = finger_pointer(data);
+bool CuckooFilter::contains(uint64_t data) const {
+  uint32_t f = get_finger_print(data);
   int ind1 = h1(data) % numBucks;
   int ind2 = (ind1 ^ h1(f)) % numBucks;
   return buckets.has(f, ind1, ind2);
 }
 
-void CuckooFilter::remove(int data) {
-  int f = finger_pointer(data);
+void CuckooFilter::remove(uint64_t data) {
+  uint32_t f = get_finger_print(data);
   int ind1 = h1(f);
   int ind2 = ind1 ^ f;
   if(buckets.check_and_remove(f, ind1))
--- CuckooFilter.h
+++ CuckooFilter.h
@@ -34,7 +34,7 @@ class CuckooFilter {
    CuckooFilter(size_t arr_size, std::shared_ptr<HashFamily> family);
   
   /**
-   * Cleans up all memory allocated by this hash table.
+   * Cleans up all memory allocated by this filter.
    */
   ~CuckooFilter();
   
@@ -47,39 +47,46 @@ class CuckooFilter {
    * have displaced an element. If it ever exceeds 6 lg n, you should trigger
    * a rehash.
    */
-  int insert(int key);
+  int insert(uint64_t data);
   
   /**
    * Returns whether the specified key is contained in this hash tasble.
    */
-  bool contains(int key) const;
+  bool contains(uint64_t data) const;
   
   /**
    * Removes the specified element from this hash table. If the element is not
    * present in the hash table, this operation is a no-op.
    */
-  void remove(int key);
+  void remove(uint64_t data);
 
   /**
    * Runs the loop of 'cuckoos' where on each iteration if no open spots,
    * a finger print  is randomly selected and evicted.  This process just
    * loops through this
    */
-  int run_cuckoo_loop(int f, int ind1, int ind2);  
+  int run_cuckoo_loop(uint32_t f, int ind1, int ind2);  
+
+
+  /**
+   * Function takes the data and gets the finger print.  After looking
+   * at the implementation in the paper, this is done by hashing the 
+   * data, then taking the finger pring from the f least significant
+   * bits of the 32 most significant bits of the hash.  So, in mem
+   * it looks like:
+   * |-- 32-f --|-- finger print --|----_ 32 least significant bits --------|
+   */
+  uint32_t get_finger_print(uint64_t data) const;
 
 private:
-  
-  /* Fun with C++: these next two lines disable implicitly-generated copy
-   * functions that would otherwise cause weird errors if you tried to
-   * implicitly copy an object of this type. You don't need to touch these
-   * lines.
-   */
-  size_t numBucks;
-  size_t num_elems;
-  int num_max_cuckoos;
-  BucketsTable buckets;
-  HashFunction h1;
-  HashFunction finger_pointer;
+  size_t numBucks;       // Number of buckets in the table
+  size_t num_elems;      // Number of elements in the table
+  int num_max_cuckoos;   // Max Cuckoo loops
+  int f_bit_size;        // Size in bits of finger print
+  uint64_t f_mask;       // Used to mask the the finger print
+  BucketsTable buckets;  // Buckets Table 
+  HashFunction h1;       // Hash function for the index
+  HashFunction finger_pointer;  // Hash Function for the finger print
   CuckooFilter(CuckooFilter const &) = delete;
   void operator=(CuckooFilter const &) = delete;
 };
